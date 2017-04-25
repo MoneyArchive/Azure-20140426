68c7f6caa135b3281f7b7f7d6cd734523bba78c5
Money <abc12207@gmail.com>
2017-04-26 05:50:21 +0800

    sample 2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    using Microsoft.Azure;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Queue;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 建立 Container
+            CreateContainer();
+
+            // 將訊息插入佇列
+            Upload();
+
+            // 查看下一個訊息
+            ListData();
+
+            // 變更佇列訊息的內容
+            Update();
+
+            // 將下一個訊息清除佇列
+            Delete();
+        }
+
+        private static void CreateContainer()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a container.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist
+            queue.CreateIfNotExists();
+        }
+
+        private static void Upload()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist.
+            queue.CreateIfNotExists();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage("Hello, World");
+            queue.AddMessage(message);
+        }
+
+        private static void ListData()
+        {
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Peek at the next message
+            CloudQueueMessage peekedMessage = queue.PeekMessage();
+
+            // Display message.
+            Console.WriteLine(peekedMessage.AsString);
+        }
+
+        private static void Update()
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Get the message from the queue and update the message contents.
+            CloudQueueMessage message = queue.GetMessage();
+            message.SetMessageContent("Updated contents.");
+            queue.UpdateMessage(message,
+                TimeSpan.FromSeconds(60.0),  // Make it invisible for another 60 seconds.
+                MessageUpdateFields.Content | MessageUpdateFields.Visibility);
+        }
+
+        private static void Delete()
+        {
+            // Retrieve storage account from connection string
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the queue client
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Get the next message
+            CloudQueueMessage retrievedMessage = queue.GetMessage();
+
+            //Process the message in less than 30 seconds, and then delete the message
+            if (retrievedMessage != null)
+                queue.DeleteMessage(retrievedMessage);
+        }
+    }
+}
